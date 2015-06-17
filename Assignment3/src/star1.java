1c376382679c872dbd69d71601c78f4e802ba44d
nibmohap <nibmohap@DIN34000328.corp.capgemini.com>
2015-06-17 17:15:11 +0530

    commit3

--- Assignment3/src/star1.java
+++ Assignment3/src/star1.java
@@ -2,20 +2,40 @@
 public class star1 {
 
 	public static void main(String[] args) {
-		int num=5;
-		for(int i=0;i<=num;i++)
+		int num=7;int mid=0;
+		if(num%2==0)
 		{
-			for(int j=0;j<=num;j++)
-			{
-				if(j%2==0)
-				{
-					System.out.print("0");
-				}else{
-					System.out.print("1");
-				}
-				
-			}System.out.println();
+			mid=num/2;
+
+		}else{
+			mid=(num+1)/2;	
 		}
+
+		for(int j=0;j<mid;j++)
+		{
+			for(int i=0;i<num;i++)
+			{
+				if(i<j || i>num-j)
+				{
+					System.out.print(" ");
+
+				}else if((j%2==0 && i%2==0) || (j%2!=0 && i%2!=0))
+				{
+					System.out.print("*");
+				}else if((j%2==0 && i%2!=0) || (j%2!=0 && i%2==0)){
+					System.out.print(" ");
+				}
+				//else if(j%2!=0 && i%2==0){
+				//	System.out.print(" ");
+				//}
+				//else if(j%2!=0 && i%2!=0){
+				//System.out.print("*");
+				//}
+
+			}System.out.println();
+
+		}
+
 	}
 
 }
