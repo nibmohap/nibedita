e3267961505ba8a68799d570d73d8a7c72ec8983
nibmohap <nibmohap@DIN34000328.corp.capgemini.com>
2015-06-17 15:39:19 +0530

    commit

---
+++ Assignment3/src/StackDemo.java
@@ -0,0 +1,14 @@
+
+public class StackDemo {
+
+	public static void main(String[] args) {
+		int[] stkArr={1,2,3,4,5,6};
+		for(int i=0;i<stkArr.length;i++)
+		{
+			System.out.print(stkArr[i]);
+		}
+		
+
+	}
+
+}
